4e01ccd79104d089644aa56c9a7f61c60588319c
Neil Sengupta <neil96sengupta@gmail.com>
2018-06-03 23:02:07 -0700

    json storage

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import os
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,7 +10,18 @@ def main():
 
 @app.route("/myoData", methods=['POST'])
 def myoData():
-	return str(request.form)
+	toStore = {}
+	toStore["gestureType"] = str(request.form["gestureType"])
+	toStore["timestamp"] = str(request.form["timestamp"])
+	toStore["imageb64"] = str(request.form["image"])
+
+	jsonString = json.dumps(toStore)
+	jsonObject = json.loads(jsonString)
+
+	with open('data.txt', 'w') as outfile:
+		json.dump(jsonObject, outfile)
+		
+	return(jsonString)
 
 if __name__ == "__main__":
     app.run(debug=True)
